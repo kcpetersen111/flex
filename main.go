e4ea11909fc7e158b5f9dcbc0b3bf33c6a108d94
Kaleb Petersen <kcpetersen@LAPTOP-6C7T2E9V.localdomain>
2022-05-24 15:06:50 -0600

    continuting to work with the file handleing

--- main.go
+++ main.go
@@ -1,64 +1,47 @@
 package main
 
 import (
-	"net"
-	"os"
+	// "net"
 	"bufio"
 	"fmt"
 	"log"
-	"time"
+	"os"
+	// "time"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
 
-	ln, err := net.Listen("tcp",":8888")
+	_, err := os.Stat("Movie")
+	if os.IsNotExist(err) {
+		os.Mkdir("Movie", 0750)
+	}
+
+	files, err := ioutil.ReadDir("Movie")
 	if err != nil {
-		log.Fatalf("Failed to start up the server, ",err)
+		log.Fatal(err)
+	}
+	movieList := make([]string, 0)
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".mkv") || strings.HasSuffix(file.Name(), ".mp4") {
+			fmt.Printf("Found movie %s\n", file.Name())
+			movieList = append(movieList, file.Name())
+		}
 	}
 
-	
-
-	conn, err := ln.Accept()
-	if err != nil{
-		fmt.Println("connection failed")
-	}
-	defer conn.Close()
-
-	log.Printf("Connection accepted ")
-
-	// fmt.Fprintf(conn, "test")
-
-	fmt.Fprintf(conn, "What video would you like to see?  ")
-	scanner := bufio.NewScanner(conn)
+	fmt.Println("What would you like to watch?")
+	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	movie := scanner.Text()
+	t := scanner.Text()
 
-	log.Printf("Streaming %s",movie)
-
-	file,err := os.Open(movie)
+	moviePath := fmt.Sprintf("Movie/%s", t)
+	movie, err := os.Stat(moviePath)
 	if err != nil {
-		fmt.Fprintf(conn, "Error in opening the movie, %v\n",err)
+		log.Fatalf("There was an error: %v\n", err)
 	}
-	defer file.Close()
-	
-	bytearr := make([]byte, 4086)
+	fmt.Printf("Movie name: %s\nMovie size: %d\nFile mode: %s\nLast modified: %s\n", movie.Name(),movie.Size(),movie.Mode(),movie.ModTime())
 
-	duration, err := time.ParseDuration("1ms")
-	if err != nil{
-		log.Fatalf("Error in parsing time buffer: %v",err)
-	}
+	return
 
-	for {
-		length, err := file.Read(bytearr)
-		if err != nil {
-			break
-		}
-		for i:=0; i<length; i++{
-			fmt.Fprintf(conn, "%b",bytearr[i])
-		}
-		time.Sleep(duration)
-	}
-
-
-	
 }
