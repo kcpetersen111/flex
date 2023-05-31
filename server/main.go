45505ef68ddb36a6f21e45874909736259c84276
Brendan <Binary141@gmail.com>
2023-05-30 20:28:15 -0600

    Refactor code and add new get route

--- server/main.go
+++ server/main.go
@@ -6,27 +6,52 @@ import (
 	"fmt"
 	"log"
 	"os"
+
 	// "time"
-	"io/ioutil"
 	"strings"
 )
 
-func main() {
+var MovieDir = "Movie"
 
-	_, err := os.Stat("Movie")
-	if os.IsNotExist(err) {
-		os.Mkdir("Movie", 0750)
-	}
-
-	files, err := ioutil.ReadDir("Movie")
+func readLocalDir() []string {
+	// return a slice of just the file names within a directory ( curently './Movie' )
+	// Might want to implement this with a parameter which is the folder name to scan
+	// and then recursively scan all directories in the movie root folder
+	dirFiles, err := os.ReadDir(MovieDir)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	files := make([]string, 0)
+	for _, val := range dirFiles {
+		files = append(files, val.Name())
+	}
+	return files
+}
+
+func isValidFile(file string) bool {
+	return (strings.HasSuffix(file, ".mkv") || strings.HasSuffix(file, ".mp4") || strings.HasSuffix(file, ".m4v"))
+}
+
+func main() {
+
+	go func() {
+		// Run the Http server
+		server()
+	}()
+
+	_, err := os.Stat(MovieDir)
+	if os.IsNotExist(err) {
+		os.Mkdir(MovieDir, 0750)
+	}
+
+	files := readLocalDir()
+
 	movieList := make([]string, 0)
 	for _, file := range files {
-		if strings.HasSuffix(file.Name(), ".mkv") || strings.HasSuffix(file.Name(), ".mp4") { //Maybe we should create an array of known filetypes that we can use for video? Or should we just try and throw it in the transcoder and see if it gives an error?
-			fmt.Printf("Found movie %s\n", file.Name())
-			movieList = append(movieList, file.Name())
+		if isValidFile(file) { // Maybe we should create an array of known filetypes that we can use for video? Or should we just try and throw it in the transcoder and see if it gives an error?
+			fmt.Printf("Found movie %s\n", file)
+			movieList = append(movieList, file)
 		}
 	}
 
@@ -35,12 +60,12 @@ func main() {
 	scanner.Scan()
 	t := scanner.Text()
 
-	moviePath := fmt.Sprintf("Movie/%s", t)
+	moviePath := fmt.Sprintf("%s/%s", MovieDir, t)
 	movie, err := os.Stat(moviePath)
 	if err != nil {
 		log.Fatalf("There was an error: %v\n", err)
 	}
-	fmt.Printf("Movie name: %s\nMovie size: %d\nFile mode: %s\nLast modified: %s\n", movie.Name(),movie.Size(),movie.Mode(),movie.ModTime())
+	fmt.Printf("Movie name: %s\nMovie size: %d\nFile mode: %s\nLast modified: %s\n", movie.Name(), movie.Size(), movie.Mode(), movie.ModTime())
 
 	return
 
--- server/server.go
+++ server/server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
+func server() {
 
 	http.Handle("/", http.FileServer(http.Dir("../website")))
+	// http.Handle("/getMovies", myHelloWorldFunc())
+	http.HandleFunc("/getMovies", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Here are the detected files! , %q", readLocalDir())
+	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
