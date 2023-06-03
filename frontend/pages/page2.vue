ccbb3a6f675e218110dce0842587f2699820b5d3
kaleb <kcpetersen111@gmail.com>
2023-06-02 21:41:14 -0600

    changed from svelte to vue with nuxt, so far have set up examples for components and routes

---
+++ frontend/pages/page2.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+        page 2
+    </header>
+    <NuxtLink to="/page1">To page 1</NuxtLink>
+        
+  </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
   theme: {
     extend: {},
   },
