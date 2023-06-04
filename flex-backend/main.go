d249e678b132ca3325f95b3c2b2e87898ab7a7b8
Brendan Davidson <binary141@gmail.com>
2023-06-04 16:28:32 -0600

    Make use of .env file for data directory as well

--- flex-backend/main.go
+++ flex-backend/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"flex/movie"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -12,12 +15,18 @@ func main() {
 	var port int
 	flag.StringVar(&sAddr, "s", "0.0.0.0", "Server Address")
 	flag.IntVar(&port, "p", 8080, "Server Port")
-	// Read env variable for the movie directory
+
+	// Read env variable for the movie directory from .env first, then look at the global environment variables
+	// If the env file doesn't exist, then let's just log the message anyways
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err)
+	}
 	path := os.Getenv("dataDir")
 
 	if len(path) == 0 {
 		// Environment variable doesn't exist, set default
-		path = "/Movie"
+		path = "/Movies"
 	}
 
 	movie := movie.NewMovie(path)
