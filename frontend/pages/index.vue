2616b22c81b71442f9fc48551c55cab876143ea1
kaleb <kcpetersen111@gmail.com>
2023-06-06 18:32:07 -0600

    basic example of how to use stores

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,4 +1,13 @@
+<!-- NEVER HAVE A useState OUTSIDE OF A <script setup> It will cause a memory leak and will be shared across all instances of the website -->
+<script setup>
+    //This will start counting at zero and every time the button is clicked it will add one
+    const counter = useState('counter', ()=> 0)
+</script>
 <template>
+    <button @click="counter++">
+        state counter
+    </button>
+    {{ counter }}
     <div>
       test <br/>
 
