357691da73c77a43070e587e494b64a06a24d391
kaleb <kcpetersen111@gmail.com>
2023-06-06 19:13:09 -0600

    another example of state management

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,17 +2,28 @@
 <script setup>
     //This will start counting at zero and every time the button is clicked it will add one
     const counter = useState('counter', ()=> 0)
+    const word = useState('word', ()=>"ahh")
 </script>
 <template>
+    <!-- state example 1 -->
     <button @click="counter++">
         state counter
     </button>
     {{ counter }}
-    <div>
-      test <br/>
+    <br/>
 
+<!-- state example two -->
+      <button @click="()=>{word='a'+word}">
+        state counter
+    </button>
+    {{ word }}
+    <br/>
+
+    <div>
+        <!-- This is a component  -->
       <example/>
-      
+
+      <!-- This is a routing example -->
       <NuxtLink to="/page1">routing example</NuxtLink>
     </div>
 </template>
