3be7a60f8658ed00d1afd7cf3cab2910067e7f05
kcp <kcpetersen111@gmail.com>
2023-08-04 23:00:12 -0600

    can now stream part of a file. still need to fix issue with parsing the frame rate

--- flex-backend/stream/main.py
+++ flex-backend/stream/main.py
@@ -1,18 +1,38 @@
 import ffmpeg
 import asyncio
 import websockets
+import numpy as np
+import json
 
-async def stream(websocket):
-    
+input_vid = 'in.mp4'
+frame_num = 0
+
+def extract_frame(input_vid, frame_num):
+    # at some point should stop ignoring the error
     out, _ = (
         ffmpeg
-        .input('in.mp4')
-        .output('pipe:', format='rawvideo', pix_fmt='rgb24')
-        .run(capture_stdout=True)
+        .input(input_vid)
+        .filter_('select', 'gte(n,{})'.format(frame_num))
+        .output('pipe:', format='rawvideo', pix_fmt='rgb24', vframes=1)
+        .run(capture_stdout=True, capture_stderr=True)
     )
+    return out
+    # return np.frombuffer(out, np.uint16).reshape(-1, height, width, 2)
 
+
+async def stream(websocket):
+
+    print(ffmpeg.probe(input_vid))
+    stats = ffmpeg.probe(input_vid)
+    
+    frameRate = stats['avg_frame_rate'].split('/')
+    frame = []
+    for i in range(frameRate[0]):
+        frame_num+=i
+        frame.append(extract_frame(input_vid,frame_num))
+   # print(video_stream)
    
-    await websocket.send(out)
+    await websocket.send(frame)
 
 async def main():
     async with websockets.serve(stream, "", 3410):
